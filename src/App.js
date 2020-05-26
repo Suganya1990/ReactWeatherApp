545732575207c8adb197090b4ab939ffce8e3935
Suganya Maheswaran <suganyamaheswaran@gmail.com>
2020-05-26 16:02:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Titles from "./components/Titles"
+import Form from "./components/Form"
+import Weather from "./components/Weather"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const API_KEY = "986f23ae0447c649c36930c44d073083"
+
+
+class App extends React.Component{
+  
+  state ={
+    temperature:undefined,
+    city: undefined,
+    country: undefined , 
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+    lat: undefined,
+    lng: undefined
+  }
+
+  componentWillMount() {
+
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({ lat: position.coords.latitude, lng: position.coords.longitude });
+    }, err => console.log(err)
+    );
+
+    console.log(this.state)
+  }
+
+  
+
+  getWeather = async (e) => 
+  {
+ 
+    e.preventDefault(); 
+
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    let api_call = undefined
+    try{
+      if(this.lat && this.lng)
+      {
+        
+        api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lng}&appid=${API_KEY}`)
+      }
+      else{
+          api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+         }
+      const data = await api_call.json();
+  
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country, 
+       humidity: data.main.humidity,
+       description: data.weather[0].description,
+       error: "",
+      })
+
+    }catch(error){
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined, 
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter correct values",
+
+      })
+    }
+
+  }
+
+  render(){
+    return(
+      <div class="wrapper">
+        <div class="main">
+          <div class="container">
+            <div class="row">
+              <div className="col-xs-5 title-container"><Titles /></div>
+              <div className="col-xs-7 form-container">
+              <Form getWeather={this.getWeather}/>
+              <Weather 
+                temperature = {this.state.temperature}
+                city= {this.state.city}
+                country={this.state.country} 
+                humidity={this.state.humidity}
+                description= {this.state.description}
+                error = {this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+
+  }
+
 }
 
-export default App;
+
+        
+
+export default App 
+
---
+++ src/components/Titles.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const Titles = ()=>{
+    return (
+        <div>
+            <h1 className="title-container__title">Weather Finder</h1>
+            <h3 className="title-container__sub_title">Find out temperature, conditions, and more... </h3>
+        </div>
+    )
+}
+
+
+
+export default Titles
